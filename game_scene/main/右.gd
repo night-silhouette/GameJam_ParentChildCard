2e4e293fca95dc18aabb8df09adf74c864b7c8d1
Napori233 <3093291282@qq.com>
2026-02-15 00:29:48 +0800

    规范代码，船的运行初步打算是先确定一个路线矩形，然后通过随机函数上下浮动

--- game_scene/main/右.gd
+++ game_scene/main/右.gd
@@ -2,15 +2,24 @@
 extends TextureRect
 var x = 576;
 @export var Duichen : TextureRect ;
+@export var animationpalyer : AnimationPlayer ;
 
+func _ready() -> void:
+	handle_animation();
 
 func _process(delta: float) -> void:
 	
+	handle_movement();
+	
 
+func handle_movement() -> void:
+				Duichen.position.x=2*x -  position.x - size.x;
+				Duichen.size.x = size.x;
+				
+				Duichen.position.y = position.y;
+				Duichen.size.y = size.y;
 	
-	Duichen.position.x=2*x -  position.x - size.x;
-	Duichen.size.x = size.x;
+func handle_animation() -> void:
 	
-	Duichen.position.y = position.y;
-	Duichen.size.y = size.y;
+	animationpalyer.play("边框进入");
 	
