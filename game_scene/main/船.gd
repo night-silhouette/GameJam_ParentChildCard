993e477f93bc0e96914c186550dae2950c091f18
Napori233 <3093291282@qq.com>
2026-02-17 00:32:23 +0800

    这是对的，再次修改，写了一个正态函数，然后写了方向转化的函数，非常的解耦，爽哉爽哉。

--- game_scene/main/船.gd
+++ game_scene/main/船.gd
@@ -1,13 +1,13 @@
 extends TextureRect
 @export var range_y : int = 34;
 @export var range_x : int = 32;
+@export var speed : float = 100;
 var mapWidth : int =1152;
 var mapHeight : int = 640;
 
 # Called when the node enters the scene tree for the first time.
 var gap_y :int = 80;#这个就是最合适的距离和边框的距离。
 var gap_x :int = 136;
-
 
 func _ready() -> void:
 	var random_x = [gap_x,mapWidth-gap_x];
@@ -24,7 +24,37 @@ func _ready() -> void:
 		position.y = random_y.pick_random();
 		rotation = deg_to_rad(0) if position.y == gap_y else deg_to_rad(180);
 		position.x = randf_range(0-size.x,mapWidth);
+		
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	pass;
+	
+func rand_normal(mean := 0.0, std_dev := 1.0):
+	var u1 = randf()
+	var u2 = randf()
+	var z0 = sqrt(-2.0 * log(u1)) * cos(TAU * u2)
+	return mean + z0 * std_dev
+	
+func change_rad():#改变角度
+	match rotation_degrees :
+		0:
+			rotation_degrees = 90;
+		90:
+			rotation_degrees = 180;
+		180:
+			rotation_degrees = 270;
+		270:
+			rotation_degrees = 0;
+		
+func decide_change_rad():#判断改变方向
+	if position.x <= 0 - size.x and rotation_degrees == 0 :
+		change_rad();
+	elif position.y <= 0 - size.x and rotation_degrees == 90 :
+		change_rad();
+	elif position.x >= mapWidth and rotation_degrees == 180 :
+		change_rad();
+	elif position.y >= mapHeight and rotation_degrees == 270 :
+		change_rad();
+		
+		
+	
