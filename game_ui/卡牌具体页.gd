030f2cced8027d9c222688274ba416db4941480d
night <631434665@qq.com>
2026-07-31 13:20:51 +0800

    卡面修改

--- game_ui/卡牌具体页.gd
+++ game_ui/卡牌具体页.gd
@@ -6,9 +6,10 @@ extends Control
 @onready var child_icon: TextureRect = $"子牌"
 @onready var hp_label: Label = $"信息标血量/hp"
 @onready var attack_label: Label = $"信息标攻击/attack"
-@onready var skill_effect_label: Label = $"信息标技能/技能效果"
-@onready var summon_condition_label: Label = $"信息标技能/召唤条件"
-@onready var summon_effect_label: Label = $"信息标技能/召唤效果"
+
+@onready var skill_effect_label: Label = $信息标技能/CenterContainer/技能效果
+
+
 @onready var cost_label: Label = $"点ui/energy"
 
 
@@ -42,12 +43,8 @@ func import_card_data(card_data: CardResource) -> void:
 	# 技能效果 / 召唤条件 / 召唤效果（仅母牌显示）
 	if is_parent:
 		skill_effect_label.text = "技能效果：" + card_data.skill_description
-		summon_condition_label.text = "召唤条件：" + card_data.notes
-		summon_effect_label.text = "召唤效果：" + card_data.sub_card_trigger_effect
+
 		skill_effect_label.visible = true
-		summon_condition_label.visible = true
-		summon_effect_label.visible = true
+
 	else:
 		skill_effect_label.visible = false
-		summon_condition_label.visible = false
-		summon_effect_label.visible = false
